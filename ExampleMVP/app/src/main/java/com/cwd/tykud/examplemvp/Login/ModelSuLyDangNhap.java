c24a67a0d2f9266f4813deca06991c035e159f59
Huy Quoc Vu <huyquocvu.sg@gmail.com>
2018-06-18 15:29:51 +0700

    - Project config change

--- ExampleMVP/app/src/main/java/com/cwd/tykud/examplemvp/Login/ModelSuLyDangNhap.java
+++
@@ -1,8 +0,0 @@
-package com.cwd.tykud.examplemvp.Login;
-
-/**
- * Created by tykud on 17/06/2018.
- */
-
-public class ModelSuLyDangNhap {
-}
--- ExampleMVP/app/src/main/java/com/cwd/tykud/examplemvp/Login/PresenterImpSuLyDangNhap.java
+++
@@ -1,9 +0,0 @@
-package com.cwd.tykud.examplemvp.Login;
-
-/**
- * Created by tykud on 17/06/2018.
- */
-
-public interface PresenterImpSuLyDangNhap {
-    void kiemTraDangNhap(String email,String pass);
-}
